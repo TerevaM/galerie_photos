a5bd7e28ba4810278b189273eed33f16d45a862e
Tereva Millet <59101444+TerevaM@users.noreply.github.com>
2022-10-29 06:54:15 +0200

    Add files via upload

---
+++ galerie_photo/compenents/pages/contact.php
@@ -0,0 +1,4 @@
+<?php
+require_once('../comp_base/comp_base.php');
+start_page('Contact','../../utils/css/');
+navbar('../../', '');
---
+++ galerie_photo/index.php
@@ -0,0 +1,9 @@
+<?php
+// import
+require_once('./compenents/comp_base/comp_base.php');
+start_page("Galerie Photo",'utils/css/');
+navbar('', './compenents/pages/');
+?>
+<script src="./utils/js/main.js"></script>
+<?php
+end_page();

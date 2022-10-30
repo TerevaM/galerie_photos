ec37f9105f06da8354dd9ed39a84efd82429dbae
Tereva Millet <59101444+TerevaM@users.noreply.github.com>
2022-10-30 16:57:06 +0100

    Delete galerie_photo directory

--- galerie_photo/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// import
-require_once('./compenents/comp_base/comp_base.php');
-start_page("Galerie Photo",'utils/css/');
-navbar('', './compenents/pages/');
-?>
-<script src="./utils/js/main.js"></script>
-<?php
-end_page();
